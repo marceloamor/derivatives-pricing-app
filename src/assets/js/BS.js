8c7b863112253be01beab5ab21ae1cef9c03c3b0
marcelo amorelli <marcelo.amorelli@upetrading.com>
2023-04-06 10:20:12 +0100

    add isBusinessMinutes function to js

--- src/assets/js/BS.js
+++ src/assets/js/BS.js
@@ -262,6 +262,38 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
       }
 
 
+      function isBusinessTime(date) {
+        const dayOfWeek = date.getUTCDay();
+        const hour = date.getUTCHours();
+        const minute = date.getUTCMinutes();
+        const isWeekday = dayOfWeek >= 1 && dayOfWeek <= 5; // Monday = 1, Friday = 5
+        const isWithinBusinessHours = hour > 9 || (hour === 9 && minute >= 45) && hour < 17 || (hour === 17 && minute <= 30);
+        return isWeekday && isWithinBusinessHours;
+      }
+
+      function countBusinessMinutesUntilExpiry(expiry, holidays) {
+        const now = new Date();
+        let count = 0;
+        let current = new Date(now);
+
+        while (current < expiry) {
+          if (isBusinessTime(current) && !holidays.includes(current.toISOString().slice(0, 10))) {
+            count++;
+          }
+          current.setUTCMinutes(current.getUTCMinutes() + 1);
+        }
+
+        return count;
+      }
+
+      // Example usage:
+      const expiryDay = new Date("2023-04-10T16:30:00Z"); // expires on April 10, 2023, at 4:30pm UTC
+      const holidays = ["2023-04-07", "2023-04-08"]; // April 7th and 8th are holidays
+      const minutesUntilExpiry = countBusinessMinutesUntilExpiry(expiry, holidays);
+      console.log(`There are ${minutesUntilExpiry} business minutes until expiry.`);
+
+
+
       //black scholes pricing function (LME)
       function bs(CoP, S, X, rc, v, T) {
         let df = Math.exp(-rc * (T));    //DELETED 14/365
