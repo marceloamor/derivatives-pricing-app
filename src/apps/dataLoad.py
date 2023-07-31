02f126708d79ca42894362f897104c4bf2d15f21
marceloupe <119584040+marceloupe@users.noreply.github.com>
2023-07-31 14:58:08 +0100

    Gf 123 lme vs rjo settle price rec (#99)
    * add rjo/lme PnL rec to dataLoad

--- src/apps/dataLoad.py
+++ src/apps/dataLoad.py
@@ -32,6 +32,7 @@ fileOptions = [
     {"label": "Rec LME Positions", "value": "rec_lme_pos"},
     {"label": "Rec CME Positions", "value": "rec_cme_pos"},
     {"label": "Rec Euronext Positions", "value": "rec_euro_pos"},
+    {"label": "Rec LME/RJO PnL", "value": "rec_settle_prices"},
 ]
 
 # layout for dataload page
@@ -68,6 +69,171 @@ layout = html.Div(
         # html.Div(id="output-rec-button"),
     ]
 )
+
+
+def recRJOstaticPNL():
+    """Calculates PnL on RJO trades and positions and compares to RJO reported PnL from cash file.
+    - Pulls reported PnL from cash file
+    - Pulls RJO positions from last two days of position files
+    - Separates T-1 trades and calculates PnL from trade price to close price
+    - Matches T-1 trades to T-2 trades and calculates PnL from T-2 close price to T-1 close price
+    - Calculates estimated fees on the day
+    - Calculates
+    """
+
+    # pull most recent cash file for pnl comparison
+    (cashdf, cash_filename) = sftp_utils.fetch_latest_rjo_export(
+        "UPETRADING_csvnmny_nmny_%Y%m%d.csv"
+    )
+    cashdf = cashdf[cashdf["Record Code"] == "M"]
+    cashdf = cashdf[cashdf["Account Number"] == "UPLME"]
+    cashdf = cashdf[cashdf["Account Type Currency Symbol"] == "USD"]
+    cashdf["PNL"] = cashdf["Liquidating Value"] - cashdf["Previous Liquidating Value"]
+    reported_pnl = (cashdf["PNL"].iloc[0]).round(2)
+
+    # pull most recent rjo position files
+    (
+        t1,
+        t1_filename,
+    ) = sftp_utils.fetch_latest_rjo_export("UPETRADING_csvnpos_npos_%Y%m%d.csv")
+
+    # fetch second latest rjo file
+    (t2, t2_filename) = sftp_utils.fetch_2nd_latest_rjo_export(
+        "UPETRADING_csvnpos_npos_%Y%m%d.csv"
+    )
+
+    # filter for exchange and record code
+    t1 = t1[t1["Bloomberg Exch Code"] == "LME"]
+    t1 = t1[t1["Record Code"] == "P"]
+
+    t2 = t2[t2["Bloomberg Exch Code"] == "LME"]
+    t2 = t2[t2["Record Code"] == "P"]
+
+    # remove yesterday's trades from t1
+    yesterday = str(
+        dt.datetime.strptime(
+            t1_filename, "UPETRADING_csvnpos_npos_%Y%m%d.csv"
+        ).strftime("%Y%m%d")
+    )
+    t1["Trade Date"] = t1["Trade Date"].astype(int)
+    t2["Trade Date"] = t2["Trade Date"].astype(int)
+
+    yday_trades = t1[t1["Trade Date"] == int(yesterday)]
+    t1 = t1[t1["Trade Date"] != yesterday]
+
+    # calculate PnL on yesterdays trades in t1
+    yday_trades["PriceDiff"] = (
+        yday_trades["Close Price"] - yday_trades["Formatted Trade Price"]
+    )
+    yday_trades["Vol"] = yday_trades.apply(
+        lambda row: row["Quantity"]
+        if int(row["Buy Sell Code"]) == 1
+        else -int(row["Quantity"]),
+        axis=1,
+    )
+    yday_trades["PnL"] = (
+        yday_trades["PriceDiff"]
+        * yday_trades["Vol"]
+        * yday_trades["Multiplication Factor"]
+    )
+    # calculate estimated fees as: sum(abs(vol)) * 3.4
+    est_fees = yday_trades["Quantity"].sum() * 3.4
+    tradesPNL = (yday_trades["PnL"].sum()).round(2)
+
+    # match t1 to t2 on security desc line 1, quantity, trade date
+    # Convert the 'Quantity' column to string data type in both DataFrames with decimal precision
+    t1["Quantity"] = t1["Quantity"].astype(str)
+    t2["Quantity"] = t2["Quantity"].astype(str)
+
+    # Merge the DataFrames on common columns and identify unmatched rows
+    combined = pd.merge(
+        t1,
+        t2,
+        on=[
+            "Buy Sell Code",
+            "Quantity",
+            "Security Desc Line 1",
+            "Trade Date",
+            "Formatted Trade Price",
+            "Multiplication Factor",
+        ],
+        how="outer",
+        suffixes=("_t1", "_t2"),
+        indicator=True,
+    )
+
+    # separate matched (pos present in t1+t2) and unmatched (pos present in either but not the other)
+    # t1 unmatched should be trades from the day
+    # t2 unmatched should be positions expiring on t1
+    matched = combined[combined["_merge"] == "both"]
+    t1_unmatched = combined[combined["_merge"] == "left_only"].drop(columns="_merge")
+    t2_unmatched = combined[combined["_merge"] == "right_only"].drop(columns="_merge")
+
+    matched = matched[
+        [
+            "Buy Sell Code",
+            "Quantity",
+            "Security Desc Line 1",
+            "Trade Date",
+            "Formatted Trade Price",
+            "Multiplication Factor",
+            "Close Price_t1",
+            "Close Price_t2",
+        ]
+    ]
+
+    # calculate PnL on these positions
+    matched["PriceDiff"] = matched["Close Price_t1"] - matched["Close Price_t2"]
+
+    # ensure relevant columns are numeric for calculations
+    matched["Quantity"] = pd.to_numeric(matched["Quantity"], errors="coerce")
+    matched["Buy Sell Code"] = pd.to_numeric(matched["Buy Sell Code"], errors="coerce")
+
+    # calculate positions and gross pnl
+    matched["Vol"] = matched.apply(
+        lambda row: int(row["Quantity"])
+        if int(row["Buy Sell Code"]) == 1
+        else int(row["Quantity"]) * -1,
+        axis=1,
+    )
+    matched["PnL"] = (
+        matched["PriceDiff"] * matched["Vol"] * matched["Multiplication Factor"]
+    )
+    matchedPNL = (matched["PnL"].sum()).round(2)
+    totalPNL = (tradesPNL + matchedPNL).round(2)
+
+    t2_unmatched["Vol"] = t2_unmatched.apply(
+        lambda row: float(row["Quantity"])
+        if int(row["Buy Sell Code"]) == 1
+        else -float(row["Quantity"]),
+        axis=1,
+    )
+    # print(t2_unmatched["Vol"].sum()) # should be 0
+
+    # build pnl table for frontend
+    table = pd.DataFrame(
+        [
+            ["T-1 Trades PnL", tradesPNL],
+            ["Pos PnL", matchedPNL],
+            ["Gross PnL", totalPNL],
+            ["Estimated Fees", est_fees],
+            ["Net PnL", totalPNL - est_fees],
+            ["Reported PnL", reported_pnl],
+            ["PnL Diff", (totalPNL - est_fees - reported_pnl).round(2)],
+        ],
+        columns=["source", "pnl"],
+    )
+
+    # build filename string for frontend
+    filename_string = (
+        "t1_filename: "
+        + t1_filename
+        + " ------- t2_filename: "
+        + t2_filename
+        + " ------- cash_filename: "
+        + cash_filename
+    )
+    return (table, filename_string)
 
 
 # function to parse data file
@@ -306,5 +472,19 @@ def initialise_callbacks(app):
                     columns=columns,
                 )
                 return table, latest_rjo_filename
+
+            elif file_type == "rec_settle_prices":
+                # column titles for output table.
+                columns = [
+                    {"id": "source", "name": "Source"},
+                    {"id": "pnl", "name": "PnL"},
+                ]
+                (table, filenames) = recRJOstaticPNL()
+                table = dtable.DataTable(
+                    id="recTable",
+                    data=table.to_dict("records"),
+                    columns=columns,
+                )
+                return table, filenames
 
         return table, filenames
