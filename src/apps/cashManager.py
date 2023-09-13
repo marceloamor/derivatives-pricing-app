17877712ece80d72d0290abb258e3297ecd0faae
marcelo amorelli <marcelo.amorelli@upetrading.com>
2023-09-13 14:08:33 +0100

    build internal pnl tool

--- src/apps/cashManager.py
+++ src/apps/cashManager.py
@@ -10,12 +10,21 @@ from data_connections import Session, conn, engine
 import upestatic
 
 from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy import text
 import traceback
 import pandas as pd
 import datetime as dt
 import numpy as np
 import pickle
+import os
 
+USE_DEV_KEYS = os.getenv("USE_DEV_KEYS", "false").lower() in [
+    "true",
+    "t",
+    "1",
+    "y",
+    "yes",
+]
 
 metals_dict = {
     "AU": "Aluminium",
@@ -24,6 +33,29 @@ metals_dict = {
     "LD": "Lead",
     "L8": "Zinc",
 }
+
+options_list = [
+    {"label": "All LME Portfolios", "value": "all"},
+    {"label": "1- LME General", "value": "1"},
+    {"label": "2- LME Carry", "value": "2"},
+]
+
+portfolio_dropdown = dbc.Row(
+    [
+        dbc.Col(
+            dcc.Dropdown(
+                id="portfolio-dropdown",
+                options=options_list,
+                value=options_list[0]["value"],
+            ),
+            width={"size": 3},
+        ),
+        dbc.Col(
+            html.Button("Refresh", id="submit-button", n_clicks=0),
+            width={"size": 2},
+        ),
+    ]
+)
 
 # layout for dataload page
 layout = html.Div(
@@ -41,13 +73,18 @@ layout = html.Div(
             type="circle",
         ),
         html.Br(),
-        html.Div(id="pnl-filestring1", children="PnL Rec Loading..."),
-        html.Div(id="pnl-filestring2", children=" "),
+        portfolio_dropdown,
+        # dcc.Dropdown(
+        #     id="portfolio-dropdown",
+        #     value=options_list[0]["value"],
+        #     options=options_list,
+        # ),
         dcc.Loading(
-            id="loading-4",
-            children=[html.Div(id="pnl-rec-table")],
+            id="loading-6",
+            children=[html.Div([html.Div(id="internal-pnl-table")])],
             type="circle",
         ),
+        html.Div(id="internal-pnl-filestring", children="Internal PnL Loading... "),
         html.Br(),
         html.Div(
             id="closePrice-rec-filestring", children="Closing Price Rec Loading... "
@@ -57,13 +94,18 @@ layout = html.Div(
             children=[html.Div([html.Div(id="closePrice-rec-table")])],
             type="circle",
         ),
-        html.Br(),
-        html.Div(id="internal-pnl-filestring", children="Internal PnL Loading... "),
+        dbc.Row(
+            [
+                html.Div(id="pnl-filestring1", children="PnL Rec Loading..."),
+            ]
+        ),
+        html.Div(id="pnl-filestring2", children=" "),
         dcc.Loading(
-            id="loading-6",
-            children=[html.Div([html.Div(id="internal-pnl-table")])],
+            id="loading-4",
+            children=[html.Div(id="pnl-rec-table")],
             type="circle",
         ),
+        # html.Br(),
     ]
 )
 
@@ -339,47 +381,270 @@ def initialise_callbacks(app):
         Output("internal-pnl-filestring", "children"),
         Output("internal-pnl-table", "children"),
         [Input("refresh-button", "n_clicks")],
+        [Input("portfolio-dropdown", "value")],
     )
-    def cashManager(n):
-        # get latest CLO file and georgia pos/trades data
-        print("hello??????")
+    def internalPnL(n, portfolio_id):
+        # first check if pnl data is in redis
+        ttl = conn.ttl("internal_pnl")
+
+        if ttl < 1:
+            print("data in redis!")
+            pnl_data = pickle.loads(conn.get("internal_pnl"))
+            if portfolio_id != "all":
+                pnl_data = pnl_data[pnl_data["portfolio_id"] == int(portfolio_id)]
+            else:
+                # aggregate pnl data
+                pnl_data = (
+                    pnl_data.groupby(["pnl_date", "metal", "product_symbol", "source"])
+                    .sum()
+                    .reset_index()
+                )
+                # turn df into dash table
+                # Pivot the DataFrame to have metals as columns and set "metal" as the index
+
+            # df = pnl_data.drop(
+            #     columns=["pnl_date", "product_symbol", "portfolio_id", "source"]
+            # )
+            # keep instead of drop
+            df = pnl_data[
+                ["metal", "t1_trades", "pos_pnl", "gross_pnl", "est_fees", "net_pnl"]
+            ]
+            # Set the index to "metal" column
+            df.set_index("metal", inplace=True)
+
+            # Transpose the DataFrame
+            pivot_df = df.T.reset_index()
+
+            # add a total column
+            pivot_df["Total"] = pivot_df.sum(axis=1, numeric_only=True)
+            pivot_df = pivot_df.round(2)
+
+            pivot_df.rename(columns={"index": "Source"}, inplace=True)
+            # rename row names in index
+
+            row_names = [
+                "T-1 Trades PnL",
+                "Pos PnL",
+                "Gross PnL",
+                "Estimated Fees",
+                "Net PnL",
+            ]
+            pivot_df.index = row_names
+
+            # now send to dash
+            table = dtable.DataTable(
+                data=pivot_df.to_dict("records"),
+                columns=[
+                    {"name": str(col_name), "id": str(col_name)}
+                    for col_name in pivot_df.columns
+                ],
+                style_data_conditional=[
+                    {
+                        "if": {
+                            "column_id": "Source",
+                        },
+                        "backgroundColor": "lightgrey",
+                    }
+                ],
+            )
+
+            return "Internal PnL", table
+
+        else:  # if not, calculate pnl
+            print("hello, i guess lets do some calculations!")
+
+        # get latest CLO files and georgia pos/trades data
         (
             clo_t1,
             clo_t1_name,
             clo_t2,
             clo_t2_name,
         ) = sftp_utils.fetch_two_latest_rjo_exports("%Y%m%d_CLO_r.csv", "/LMEPrices")
-        print(clo_t1.head())
-        print(clo_t1_name)
-        print(clo_t2.head())
-        print(clo_t2_name)
+
         # get date object from file names
         t1_date = dt.datetime.strptime(clo_t1_name, "%Y%m%d_CLO_r.csv").date()
         t2_date = dt.datetime.strptime(clo_t2_name, "%Y%m%d_CLO_r.csv").date()
-
         print(t1_date, t2_date)
-        # get clo
+        # this is a random
 
         # get georgia pos
         with engine.connect() as cnxn:
             positions = pd.read_sql_table("positions", cnxn)
-            trades = pd.read_sql_table("trades", cnxn)
+            stmt = f"SELECT * FROM trades WHERE deleted = false and trade_datetime_utc > '{t1_date}'"
+            trades = pd.read_sql(stmt, cnxn)
 
-        # print(positions.head(50))
-        # print(trades.head(50))
-        trades["date"] = pd.to_datetime(trades["trade_datetime_utc"])
-        t1_trades = trades[trades["trade_datetime_utc"] == t1_date]
-        print(t1_trades.head(50))
-        # positions.to_csv("positions.csv")
-        # get georgia trades
+        # format data
+        trades["date"] = trades["trade_datetime_utc"].dt.date
+        positions["instrument_symbol"] = positions["instrument_symbol"].str.upper()
 
-        # sql = "SELECT*  FROM trades where dateTime > '" + date + "' order by dateTime desc"
-        # df = pd.read_sql_query(sql, cnxn)
-        # write a matching function to fetch the correct price from clo for any instrument
+        # this is where the ideal split woul be
+        pnl_per_portfolio = {}
 
-        # separate t-1 trades and calc pnl
-        # separate t-2 by subtracting t-1 and t-0 trades and calc pnl
-        return "hello!", "world!"
+        def calc_pnl_per_portfolio(portfolio_id):
+            positions_portfolio = positions[positions["portfolio_id"] == portfolio_id]
+            trades_portfolio = trades[trades["portfolio_id"] == portfolio_id]
+
+            def calc_pnl_per_metal(metal):
+                # filter pos and trades for metal
+                positions_metal = positions_portfolio[
+                    positions_portfolio["instrument_symbol"].str.contains(metal)
+                ]
+                trades_metal = trades_portfolio[
+                    trades_portfolio["instrument_symbol"].str.contains(metal)
+                ]
+
+                # calc t1_trades pnl and est_fees
+                t1_trades = trades_metal[trades_metal["date"] == t1_date]
+                t0_trades = trades_metal[trades_metal["date"] != t1_date]
+
+                if not t1_trades.empty:
+                    print("t1 trades:")
+                    t1_trades = get_prices_from_clo(t1_trades, clo_t1, "t1")
+                    t1_trades["price_diff"] = t1_trades["t1_price"] - t1_trades["price"]
+                    t1_trades["mult"] = t1_trades["instrument_symbol"].apply(
+                        lambda x: 25 if x[:3] != "LND" else 6
+                    )
+                    t1_trades["pnl"] = (
+                        t1_trades["price_diff"]
+                        * t1_trades["quantity"]
+                        * t1_trades["mult"]
+                    )
+                    est_fees = t1_trades["quantity"].abs().sum() * 3.4
+                    t1_trades_pnl = t1_trades["pnl"].sum()
+                else:
+                    t1_trades_pnl = 0
+                    est_fees = 0
+
+                # get positions as they were at t2 close
+                aggregated_trades = (
+                    trades.groupby("instrument_symbol")["quantity"].sum().reset_index()
+                )
+                net_new_trades = dict(
+                    zip(
+                        aggregated_trades["instrument_symbol"],
+                        aggregated_trades["quantity"],
+                    )
+                )
+                t2_positions = positions_metal.copy()
+                # update net_quantity for each product traded in last two days
+                for index, row in t2_positions.iterrows():
+                    symbol = row["instrument_symbol"]
+                    if symbol in net_new_trades:
+                        t2_positions.at[index, "net_quantity"] -= net_new_trades[symbol]
+                t2_positions = t2_positions[t2_positions["net_quantity"] != 0]
+                print("t2pos:", t2_positions)
+                if not t2_positions.empty:
+                    # get t1 and t2 settle prices from lme files
+                    print("t2 positions:")
+                    t2_positions = get_prices_from_clo(t2_positions, clo_t2, "t2")
+                    print("t1 positions:")
+                    t2_positions = get_prices_from_clo(t2_positions, clo_t1, "t1")
+
+                    # calculate pnl on positions
+                    t2_positions["price_diff"] = (
+                        t2_positions["t1_price"] - t2_positions["t2_price"]
+                    )
+                    # set multiplier manually!!
+                    t2_positions["mult"] = t2_positions["instrument_symbol"].apply(
+                        lambda x: 25 if x[:3] != "LND" else 6
+                    )
+                    t2_positions["pnl"] = (
+                        t2_positions["price_diff"]
+                        * t2_positions["net_quantity"]
+                        * t2_positions["mult"]
+                    )
+                    pos_pnl = t2_positions["pnl"].sum()
+                else:
+                    pos_pnl = 0
+                gross_pnl = t1_trades_pnl + pos_pnl
+                net_pnl = gross_pnl - est_fees
+
+                results = [t1_trades_pnl, pos_pnl, gross_pnl, est_fees, net_pnl]
+                print(metal, results)
+                return results
+
+            metals = {
+                "LZH": "Zinc",
+                "LND": "Nickel",
+                "LAD": "Aluminium",
+                "LCU": "Copper",
+                "PBD": "Lead",
+            }
+
+            pnl_per_product = {}
+            for symbol, metal in metals.items():
+                # handle unexepected products in rec
+
+                pnl_per_product[metal] = calc_pnl_per_metal(symbol)
+                # print(metal, pnl_per_product[metal])
+
+            return pnl_per_product
+
+        # portfolios = [1, 2]
+
+        portfolio_1 = calc_pnl_per_portfolio(1)
+        portfolio_2 = calc_pnl_per_portfolio(2)
+
+        print(portfolio_1)
+        print(portfolio_2)
+
+        # Convert the dictionaries into dataframes with "source" column added
+        df1 = pd.DataFrame.from_dict(
+            portfolio_1,
+            orient="index",
+            columns=["t1_trades", "pos_pnl", "gross_pnl", "est_fees", "net_pnl"],
+        )
+        df1["metal"] = df1.index
+        df1["source"] = "Georgia"
+        df1["portfolio_id"] = 1
+
+        df2 = pd.DataFrame.from_dict(
+            portfolio_2,
+            orient="index",
+            columns=["t1_trades", "pos_pnl", "gross_pnl", "est_fees", "net_pnl"],
+        )
+        df2["metal"] = df2.index
+        df2["source"] = "Georgia"
+        df2["portfolio_id"] = 2
+
+        # Concatenate the dataframes
+        final_df = pd.concat([df1, df2], ignore_index=True)
+        final_df["pnl_date"] = t1_date
+
+        metals_symbol_dict = {
+            "Aluminium": "xlme-lad-usd",
+            "Copper": "xlme-lcu-usd",
+            "Nickel": "xlme-lnd-usd",
+            "Lead": "xlme-pbd-usd",
+            "Zinc": "xlme-lzh-usd",
+        }
+
+        # Reorder the columns
+        final_df = final_df[
+            [
+                "pnl_date",
+                "portfolio_id",
+                "metal",
+                "source",
+                "t1_trades",
+                "pos_pnl",
+                "gross_pnl",
+                "est_fees",
+                "net_pnl",
+            ]
+        ]
+        final_df["product_symbol"] = final_df["metal"].map(metals_symbol_dict)
+
+        print(final_df)
+
+        # ex keyword argument in conn.set , check docs
+        # first run of day store in redis, 10hr timeout
+        # store pnl data in redis for 12hrs to avoid re-running pnl calculations
+        conn.set("internal_pnl", pickle.dumps(final_df), ex=60 * 60 * 12)
+
+        # send to postgres as well
+
+        return "hello", "world"
 
 
 def get_product_pnl(t1, t2, yesterday, product):
@@ -493,6 +758,117 @@ def get_product_pnl(t1, t2, yesterday, product):
         "BN": "xlme-lnd-usd",
         "LD": "xlme-pbd-usd",
         "L8": "xlme-lzh-usd",
+    }
+
+    results_dict = {
+        "pnl_date": date,
+        "product_symbol": metals_dict_db[product],
+        "source": "RJO",
+        "t1_trades": tradesPNL,
+        "pos_pnl": matchedPNL,
+        "gross_pnl": totalPNL,
+    }
+
+    # send to db
+    stmt = (
+        insert(upestatic.ExternalPnL)
+        .values(**results_dict)
+        .on_conflict_do_update(
+            index_elements=["pnl_date", "product_symbol", "source"],
+            set_=results_dict,
+        )
+    )
+
+    with Session() as session:
+        session.execute(stmt)
+        session.commit()
+
+    return results
+
+
+def get_prices_from_clo(t2_pos, clo_df, day):
+    # make a matching function
+    def get_price_from_clo(row):
+        price = -1
+        instrument = row["instrument_symbol"]
+        isOption = True if row["instrument_symbol"][-1] in ["C", "P"] else False
+
+        metals_dict_CLO = {
+            "LZH": "ZS",
+            "LND": "NI",
+            "LAD": "AH",
+            "LCU": "CA",
+            "PBD": "PB",
+        }
+
+        months = {
+            "F": "01",
+            "G": "02",
+            "H": "03",
+            "J": "04",
+            "K": "05",
+            "M": "06",
+            "N": "07",
+            "Q": "08",
+            "U": "09",
+            "V": "10",
+            "X": "11",
+            "Z": "12",
+        }
+
+        if not isOption:
+            product, prompt = instrument.split(" ")
+            prompt = prompt.replace("-", "")
+
+            clo_filtered = clo_df[
+                (clo_df["UNDERLYING"] == metals_dict_CLO[product])
+                & (clo_df["CONTRACT"] == metals_dict_CLO[product] + "D")
+                & (clo_df["CONTRACT_TYPE"] == "LMEForward")
+                & (clo_df["FORWARD_DATE"] == int(prompt))
+            ]
+            # price = clo_filtered.iloc[0]["PRICE"]
+
+        elif isOption:
+            instrument, strike, cop = instrument.split(" ")
+            product, month, year = instrument[:3], instrument[-2], instrument[-1]
+            expiry = "202" + year + months[month]
+
+            clo_filtered = clo_df[
+                (clo_df["UNDERLYING"] == metals_dict_CLO[product])
+                & (clo_df["CONTRACT_TYPE"] == "LMEOption")
+                & (clo_df["FORWARD_MONTH"] == int(expiry))
+                & (clo_df["STRIKE"] == int(strike))
+                & (clo_df["SUB_CONTRACT_TYPE"] == cop)
+            ]
+
+        if clo_filtered.empty:
+            print("No price found for: ", row["instrument_symbol"])
+            price = 0
+        else:
+            price = clo_filtered.iloc[0]["PRICE"]
+
+        return price
+
+    col_name = day + "_price"
+    t2_pos[col_name] = t2_pos.apply(get_price_from_clo, axis=1)
+
+    return t2_pos
+
+
+def send_pnl_to_dbs(port_1, port_2):
+    """Sends pnl data to postgres db and redis timed key"""
+    # build and send data to postgres
+    # data to send: date, product, t1-trades, pos_pnl, gross_pnl
+    date = dt.datetime.strptime(yesterday, "%Y%m%d").date()
+
+    results = [tradesPNL, matchedPNL, totalPNL, est_fees, netPNL]
+
+    metals_dict_db = {
+        "Aluminium": "xlme-lad-usd",
+        "Copper": "xlme-lcu-usd",
+        "Nickel": "xlme-lnd-usd",
+        "Lead": "xlme-pbd-usd",
+        "Zinc": "xlme-lzh-usd",
     }
 
     results_dict = {
