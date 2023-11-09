56850d441e9f96a17cc81f1fe7b27196db421bd0
marceloupe <119584040+marceloupe@users.noreply.github.com>
2023-11-09 17:34:24 +0000

    add live m2m (hope it works) (#124)

--- src/apps/m2m_rec.py
+++ src/apps/m2m_rec.py
@@ -1,5 +1,7 @@
 from parts import topMenu, multiply_rjo_positions
-from data_connections import engine
+from data_connections import engine, conn
+
+from apps.cashManager import expiry_from_symbol
 import sftp_utils
 
 import upestatic
@@ -10,8 +12,25 @@ from dash import dash_table as dtable
 from dash import dcc, html
 import dash_daq as daq
 import pandas as pd
+import numpy as np
 
 import datetime as dt
+import json
+
+monthCode = {
+    1: "f",
+    2: "g",
+    3: "h",
+    4: "j",
+    5: "k",
+    6: "m",
+    7: "n",
+    8: "q",
+    9: "u",
+    10: "v",
+    11: "x",
+    12: "z",
+}
 
 
 columns = [
@@ -81,8 +100,8 @@ options = dbc.Row(
     [
         dbc.Col(html.Div(children=[exchangeLabel, exchangeDropdown]), width=3),
         dbc.Col(html.Div(children=[shockLabel, shockSlider]), width=3),
-        # dbc.Col(html.Div(children=[liveLabel, liveSwitch]), width=1),
-        dbc.Col(html.Div(children=""), width=4),
+        dbc.Col(html.Div(children=[liveLabel, liveSwitch]), width=1),
+        dbc.Col(html.Div(children=""), width=3),
         dbc.Col(html.Div(children=[rateLabel, rateInput]), width=2),
     ]
 )
@@ -210,41 +229,53 @@ def initialise_callbacks(app):
     @app.callback(
         Output("m2m-rec-table", "data"),
         Output("m2m-rec-table", "style_data_conditional"),
-        [Input("m2m-rec-store", "data"), Input("shockSlider", "value")],
+        [
+            Input("m2m-rec-store", "data"),
+            Input("shockSlider", "value"),
+            Input("live-store", "data"),
+            Input("static_live", "on"),
+        ],
     )
-    def display_m2m(data, value):
-        if data:
-            if value == -5:
-                data = pd.DataFrame(data)
-                data["market_value"] = data["mv_down5"]
-                data["market_value_cum"] = data["market_value_cum_down5"]
-                data = data.drop(columns=["mv_down5", "mv_up5", "market_value_cum_up5"])
-            elif value == 5:
-                data = pd.DataFrame(data)
-                data["market_value"] = data["mv_up5"]
-                data["market_value_cum"] = data["market_value_cum_up5"]
-                data = data.drop(
-                    columns=["mv_down5", "mv_up5", "market_value_cum_down5"]
-                )
-            elif value == 0:
-                data = pd.DataFrame(data)
-                data = data.drop(
-                    columns=[
-                        "mv_down5",
-                        "mv_up5",
-                        "market_value_cum_down5",
-                        "market_value_cum_up5",
-                    ]
-                )
+    def display_m2m(data, value, live_data, on):
+        if not on:
+            if data:
+                if value == -5:
+                    data = pd.DataFrame(data)
+                    data["market_value"] = data["mv_down5"]
+                    data["market_value_cum"] = data["market_value_cum_down5"]
+                    data = data.drop(
+                        columns=["mv_down5", "mv_up5", "market_value_cum_up5"]
+                    )
+                elif value == 5:
+                    data = pd.DataFrame(data)
+                    data["market_value"] = data["mv_up5"]
+                    data["market_value_cum"] = data["market_value_cum_up5"]
+                    data = data.drop(
+                        columns=["mv_down5", "mv_up5", "market_value_cum_down5"]
+                    )
+                elif value == 0:
+                    data = pd.DataFrame(data)
+                    data = data.drop(
+                        columns=[
+                            "mv_down5",
+                            "mv_up5",
+                            "market_value_cum_down5",
+                            "market_value_cum_up5",
+                        ]
+                    )
 
-            styles = discrete_background_color_bins(data)
+                styles = discrete_background_color_bins(data)
+        else:
+            if live_data:
+                data = pd.DataFrame(live_data)
+                styles = discrete_background_color_bins(data)
 
             # data["market_value"] = data["market_value"].apply(format_with_commas)
             # data["market_value_cum"] = data["market_value_cum"].apply(
             #     format_with_commas
             # )
             # handle non lme exchanges for now
-            return data.round(0).to_dict("records"), styles
+        return data.round(0).to_dict("records"), styles
 
     @app.callback(
         Output("discount-table", "data"),
@@ -366,23 +397,174 @@ def initialise_callbacks(app):
             # handle non lme exchanges for now
         return new_data.round(0).to_dict("records"), style
 
-    # @app.callback(
-    #     Output("live-store", "data"),
-    #     # Output("discount-table", "style_data_conditional"),
-    #     [Input("m2m-rec-store", "data"), Input("static_live", "on")],
-    # )
-    # def live_m2m(data, liveSwitch):
-    #     # alright what do I need:
-    #     # pull list of all our trades
-    #     # compile them down into a dataframe of expiries, quantities, avg price paid
-    #     # ge
+    @app.callback(
+        Output("live-store", "data"),
+        # Output("m2m-rec-table", "data"),
+        # Output("m2m-rec-table", "style_data_conditional"),
+        # [Input("m2m-rec-store", "data"), Input("static_live", "on")],
+        [Input("exchanges", "value")],
+    )
+    def live_m2m(data):
+        # alright what do I need:
+        # pull list of all our trades
+        # compile them down into a dataframe of expiries, quantities, avg price paid
+        #
+        # get 3m date, month, and associated key
+        three_m = str(json.loads(conn.get("3m")))
+        third_m_date = dt.datetime.strptime(three_m, "%Y%m%d")
+        third_month_code = monthCode[third_m_date.month] + str(third_m_date.year)[-1]
 
-    #     # SELECT * FROM public.lme_positions_from_trades
-    #     sql = "SELECT * FROM public.lme_positions_from_trades"
-    #     df = pd.read_sql(sql, engine)
-    #     print(df)
+        # SELECT * FROM public.lme_positions_from_trades
+        sql = "SELECT * FROM public.lme_positions_from_trades"
+        df = pd.read_sql(sql, engine)
+        #
 
-    #     return ""
+        # add a metals column
+        df["product"] = df["instrument_symbol"].str.slice(start=0, stop=3)
+
+        # add expiry column
+        df["expiry"] = df["instrument_symbol"].apply(expiry_from_symbol)
+
+        # filter for past expiry
+        df = df[df["expiry"] > dt.date.today()]
+
+        # add a cop column
+        df["cop"] = df["instrument_symbol"].apply(
+            lambda x: "C" if x[-1] == "C" else ("P" if x[-1] == "P" else "F")
+        )
+        # Apply the function to create the 'strike' column
+        df["strike"] = df.apply(get_strike_from_symbol, axis=1)
+
+        df["price"] = -1
+
+        # # split based on product
+        grouped_by_product = df.groupby("product")
+        dfs = []
+
+        # Process each group
+        for product, group_df in grouped_by_product:
+            # pull fcp curve
+            fcp = json.loads(conn.get(f"lme:xlme-{product.lower()}-usd:fcp:dev"))
+
+            # pull option key for 3m
+            params = json.loads(conn.get(f"{product.lower()}o{third_month_code}"))
+            params = pd.DataFrame.from_dict(params, orient="index")
+            underlying_price = params.iloc[0]["und_calc_price"]
+
+            third_wed = (
+                dt.datetime.fromtimestamp(params.iloc[0]["third_wed"] / 1e9)
+                .date()
+                .strftime("%Y%m%d")
+            )
+
+            fcp_third_wed = fcp[third_wed]
+
+            spread = underlying_price - fcp_third_wed
+
+            # GET THE SPREAD!!!!!
+
+            # Split by cop
+            cop_groups = group_df.groupby("cop")
+
+            # Process each cop group
+            for cop, cop_group_df in cop_groups:
+                if cop == "F":
+                    # Get price for 'F' cop
+                    for index, row in cop_group_df.iterrows():
+                        try:
+                            fcp_price = fcp[str(row["expiry"]).replace("-", "")]
+                            cop_group_df.at[index, "price"] = fcp_price + spread
+                        except:
+                            cop_group_df.at[index, "price"] = 0
+                            print("didnt find it!")
+
+                    #     cop_group_df["price"] = spread
+                    # cop_group_df["price"] = cop_group_df.apply(
+                    #     lambda row: fcp.get(row["expiry"], 0) + spread, axis=1
+                    # )
+                    # # cop_group_df["price"] = cop_group_df["expiry"].apply(
+                    # #     lambda x: fcp.get(x, 0) + spread
+                    # # )
+                    dfs.append(cop_group_df)
+                else:
+                    # split by expiry for other cop values
+                    expiry_groups = cop_group_df.groupby("expiry")
+
+                    # list to store individual expiry DataFrames
+                    expiry_dfs = []
+
+                    # process each expiry group
+                    for expiry, expiry_group_df in expiry_groups:
+                        option = (
+                            expiry_group_df.iloc[0]["instrument_symbol"]
+                            .lower()
+                            .split(" ")[0]
+                        )
+                        opt_params = json.loads(conn.get(option))
+
+                        # group by strike
+                        strike_groups = expiry_group_df.groupby("strike")
+
+                        # list to store individual strike DataFrames
+                        strike_dfs = []
+
+                        # process each strike group
+                        for strike, strike_group_df in strike_groups:
+                            index_to_find = f"{option} {strike} {cop.lower()}"
+                            param_row = dict(opt_params[index_to_find])
+                            price = param_row["calc_price"]
+                            strike_group_df["price"] = price
+                            strike_dfs.append(strike_group_df)
+
+                        # concatenate strike groups back together
+                        expiry_group_df = pd.concat(strike_dfs)
+                        expiry_dfs.append(expiry_group_df)
+
+                    # concatenate expiry groups back together
+                    cop_group_df = pd.concat(expiry_dfs)
+
+                    # append cop group to the list of DataFrames
+                    dfs.append(cop_group_df)
+
+        # concatenate all groups back together
+        result_df = pd.concat(dfs)
+
+        # mv for options = vol * mult * price
+        # mv for futures = vol * mult * (tradedP - price)
+        result_df["mult"] = result_df.apply(
+            lambda row: 6 if row["product"] == "LND" else 25, axis=1
+        )
+
+        condition_cop_cp = (result_df["cop"] == "C") | (result_df["cop"] == "P")
+
+        # Calculate market_value based on the condition
+        result_df["market_value"] = np.where(
+            condition_cop_cp,
+            result_df["net_quantity"] * result_df["mult"] * result_df["price"],
+            (result_df["price"] - result_df["vol_weighted_avg_price"])
+            * result_df["net_quantity"]
+            * result_df["mult"],
+        )
+
+        # print(result_df.to_string())
+
+        result_df = result_df[
+            [
+                "expiry",
+                "market_value",
+            ]
+        ]
+
+        # aggregate by expiry, summing market value
+        df = result_df.groupby(["expiry"]).sum()
+        df = df.reset_index()
+
+        # # add cumulative market value
+        df["market_value_cum"] = df["market_value"][::-1].cumsum()[::-1]
+
+        # styles = discrete_background_color_bins(df)
+
+        return df.round(0).to_dict("records")
 
 
 # colour bins for market value column, and highlight min cumulative value
@@ -511,3 +693,11 @@ def format_with_commas(x):
         return "{:,.0f}".format(x)
     else:
         return x
+
+
+def get_strike_from_symbol(row):
+    if row["cop"] == "F":
+        return 0
+    else:
+        # Split the instrument_symbol and get the second word
+        return row["instrument_symbol"].split()[1]
