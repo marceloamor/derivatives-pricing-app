14cf0d3a9a0e4ecd0327b7a1c7fc03b43af95c86
fwf-upetrading <frederick.fillingham@upetrading.com>
2024-02-23 10:00:49 +0000

    Add proper grouping

--- src/apps/portfolio.py
+++ src/apps/portfolio.py
@@ -58,7 +58,7 @@ interval = str(1000 * 1)
 #     {"name": "Gamma Breakeven", "id": "total_gammaBreakEven"},
 # ]
 columns = [
-    {"name": "Product", "id": "instrument_symbol"},
+    {"name": "Product", "id": "derivative_symbol"},
     {"name": "Portfolio", "id": "portfolio_name"},
     {"name": "Delta", "id": "total_deltas"},
     {"name": "Full Delta", "id": "total_skew_deltas"},
@@ -177,12 +177,28 @@ def initialise_callbacks(app):
             # df = df.groupby(["instrument_symbol", "portfolio_id"], as_index=False).sum(
             #     numeric_only=True
             # )
-            print(df.columns)
             # create new column, product, from the first word in the instrument_symbol
-            df["product"] = df["instrument_symbol"].str.split(" ").str[0]
+            df["split_symbol"] = df["instrument_symbol"].str.split(" ")
+
+            df["product"] = df["split_symbol"].str[0]
+            df.loc[df["contract_type"] != "o", "derivative_symbol"] = df.loc[
+                df["contract_type"] != "o", "split_symbol"
+            ].str.join(" ")
+            df.loc[df["contract_type"] == "o", "derivative_symbol"] = (
+                df.loc[df["contract_type"] == "o", "split_symbol"]
+                .str[0:3]
+                .str.join(" ")
+                .str.cat(
+                    df.loc[df["contract_type"] == "o", "split_symbol"]
+                    .str[-1]
+                    .str.split("-")
+                    .str[0],
+                    sep=" ",
+                )
+            )
             df = df.groupby(
                 [
-                    "instrument_symbol",
+                    "derivative_symbol",
                     "product",
                     "contract_type",
                     "expiry_date",
@@ -190,6 +206,9 @@ def initialise_callbacks(app):
                 ],
                 as_index=False,
             ).sum(numeric_only=True)
+            df.loc[:, "contract_expiry"] = pd.to_datetime(
+                df["derivative_symbol"].str.split(" ").str[2], format=r"%y-%m-%d"
+            )
             # create new column, portfolio_name, from the portfolio_id and port_dict
             df["portfolio_name"] = df["portfolio_id"].map(port_dict)
 
@@ -219,7 +238,7 @@ def initialise_callbacks(app):
             )
 
             # # sort on expiry --- column called 't_to_expirt' from pos_eng
-            df.sort_values("t_to_expiry", inplace=True)
+            df.sort_values("contract_expiry", inplace=True)
             # df.sum(numeric_only=True, axis=0)
 
             # # sort by date
@@ -228,83 +247,10 @@ def initialise_callbacks(app):
 
             # calc total row and re label
             df.loc["Total"] = df.sum(numeric_only=True, axis=0)
-            df.loc["Total", "instrument_symbol"] = "Total"
+            df.loc["Total", "derivative_symbol"] = "Total"
 
             # still need to finish this i believe
             return df.round(3).to_dict("records")
-
-        if portfolio == "xext-ebm-eur":
-            # pull list of porducts from static data
-            data = conn.get(positionLocationEUR).decode("utf-8")
-            if data != None:
-                dff = pd.read_json(data)
-                # aggregate by product name
-                dff = dff.groupby("contract_symbol", as_index=False).sum(
-                    numeric_only=True
-                )
-                dff["multiplier"] = 50
-                dff["total_gammaBreakEven"] = 0.0
-                valid_befg_df = dff.loc[
-                    (dff["total_fullGamma"] * dff["total_theta"] < 0.0)
-                    & (dff["total_fullGamma"].abs() > 1e-6),
-                    :,
-                ]
-
-                dff.loc[
-                    (dff["total_fullGamma"] * dff["total_theta"] < 0.0)
-                    & (dff["total_fullGamma"].abs() > 1e-6),
-                    "total_gammaBreakEven",
-                ] = np.sqrt(
-                    -2
-                    * valid_befg_df["total_theta"]
-                    / (valid_befg_df["multiplier"] * valid_befg_df["total_fullGamma"])
-                )
-
-                # sort on expiry
-                dff.sort_values("T_cal_to_underlying_expiry", inplace=True)
-                dff.sum(numeric_only=True, axis=0)
-
-                # sort by date
-                dff["expiry"] = dff["contract_symbol"].str.split(" ").str[2]
-                dff.sort_values("expiry", inplace=True, ascending=True)
-
-                # calc total row and re label
-                dff.loc["Total"] = dff.sum(numeric_only=True, axis=0)
-                dff.loc["Total", "contract_symbol"] = "Total"
-
-                return dff.round(3).to_dict("records")
-        else:
-            dff = conn.get(positionLocationLME).decode("utf-8")
-            dff = pd.read_json(dff)
-
-            if not dff.empty:
-                # sort on expiry
-                dff.sort_values("expiry", inplace=True)
-                # group on product
-                dff = (
-                    dff[dff["portfolio"] == product]
-                    .groupby("product")
-                    .sum(numeric_only=True)
-                    .round(3)
-                    .reset_index()
-                )
-                dff["total_gammaBreakEven"] = 0.0
-
-                # sort based on product name
-                dff[["first_value", "last_value"]] = dff["product"].str.extract(
-                    r"([ab])?(\d)"
-                )
-                dff = dff.sort_values(by=["first_value", "last_value"])
-                dff.drop(columns=["last_value", "first_value"], inplace=True)
-
-                # calc total row and re label
-                dff.loc["Total"] = dff.sum(numeric_only=True, axis=0)
-                dff.loc["Total", "product"] = "Total"
-
-                # rename product to match euronext
-                dff.rename(columns={"product": "contract_symbol"}, inplace=True)
-
-                return dff.round(3).to_dict("records")
 
     @app.callback(Output("ring", "children"), [Input("live-update", "n_intervals")])
     def updareRing(interval):
