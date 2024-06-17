dc5f70c0a5b55577816227553f29c15a9390f34a
fwf-upetrading <frederick.fillingham@upetrading.com>
2024-06-17 14:45:52 +0100

    Enforce typing on booking

--- src/apps/lme_carry.py
+++ src/apps/lme_carry.py
@@ -365,7 +365,7 @@ def gen_2_year_monthly_pos_table():
     today_date = now_dt.date()
 
     prebuilt_data = []
-    for i in range(18):
+    for i in range(20):
         prebuilt_data.append(
             {
                 "id": (today_date + relativedelta(months=i)).strftime(r"%b-%y"),
@@ -1374,8 +1374,8 @@ def initialise_callbacks(app):
                 sql_utils.TradesTable(
                     trade_datetime_utc=booking_dt,
                     instrument_symbol=trade_row["Instrument"].lower(),
-                    quantity=trade_row["Qty"],
-                    price=trade_row["Basis"],
+                    quantity=int(trade_row["Qty"]),
+                    price=float(trade_row["Basis"]),
                     portfolio_id=trade_row["Account ID"],
                     trader_id=trader_id,
                     notes="Carry Page",
